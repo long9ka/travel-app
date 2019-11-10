9492f5252d7f41fa403852016e7a612f1dbfd849
Long9ka <long9ka@gmail.com>
2019-11-10 08:42:06 +0700

    Make Register

--- app/src/main/java/com/example/travelapp/api/service/UserService.java
+++ app/src/main/java/com/example/travelapp/api/service/UserService.java
@@ -1,8 +1,12 @@
 package com.example.travelapp.api.service;
 
 import com.example.travelapp.api.model.RequestModels.Login;
+import com.example.travelapp.api.model.RequestModels.LoginFacebook;
+import com.example.travelapp.api.model.RequestModels.Register;
 import com.example.travelapp.api.model.ResponseModels.Sucess.UserInfo;
 import com.example.travelapp.api.model.ResponseModels.Sucess.UserLogin;
+import com.example.travelapp.api.model.ResponseModels.Sucess.UserLoginFacebook;
+import com.example.travelapp.api.model.ResponseModels.Sucess.UserRegister;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -17,4 +21,10 @@ public interface UserService {
 
     @GET("/user/info")
     Call<UserInfo> auth(@Header("Authorization") String token);
+
+    @POST("/user/register")
+    Call<UserRegister> register(@Body Register register);
+
+    @POST("/user/login/by-facebook")
+    Call<UserLoginFacebook> loginFacebook(@Body LoginFacebook loginFacebook);
 }
--- app/src/main/java/com/example/travelapp/ui/RegisterActivity.java
+++ app/src/main/java/com/example/travelapp/ui/RegisterActivity.java
@@ -5,11 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.travelapp.R;
+import com.example.travelapp.api.model.RequestModels.Register;
+import com.example.travelapp.api.model.ResponseModels.Sucess.UserRegister;
+import com.example.travelapp.api.service.ApiUtils;
+import com.example.travelapp.api.service.UserService;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private UserService userService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +42,63 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
+        final EditText fullNameEditText = findViewById(R.id.full_name);
+        final EditText emailEditText = findViewById(R.id.email);
+        final EditText phoneEditText = findViewById(R.id.phone);
+        final EditText addressEditText = findViewById(R.id.address);
+        final EditText genderEditText = findViewById(R.id.gender);
+        final EditText dobEditText = findViewById(R.id.dob);
+        final EditText passwordEditText = findViewById(R.id.password);
+        Button registerButton = findViewById(R.id.register);
 
+        userService = ApiUtils.getUserService();
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register(
+                        emailEditText.getText().toString(),
+                        phoneEditText.getText().toString(),
+                        passwordEditText.getText().toString(),
+                        fullNameEditText.getText().toString(),
+                        addressEditText.getText().toString(),
+                        genderEditText.getText().toString(),
+                        dobEditText.getText().toString()
+                );
+            }
+        });
+
+    }
+
+    private void register(String email, String phone, String password, String fullName, String address, String gender, String birthday) {
+        Call<UserRegister> call = userService.register(new Register(email, phone, password, fullName, address, gender, birthday));
+        call.enqueue(new Callback<UserRegister>() {
+            @Override
+            public void onResponse(Call<UserRegister> call, Response<UserRegister> response) {
+                if (response.isSuccessful()) {
+                    callBackLogin();
+                } else {
+                    try {
+                        JSONObject jsonObject = new JSONObject(response.errorBody().string());
+                        String msg = jsonObject.getJSONArray("message").getJSONObject(0).get("msg").toString();
+                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<UserRegister> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), R.string.register_failed, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void callBackLogin() {
+        Toast.makeText(getApplicationContext(), R.string.register_success, Toast.LENGTH_SHORT).show();
+        finish();
     }
 }
