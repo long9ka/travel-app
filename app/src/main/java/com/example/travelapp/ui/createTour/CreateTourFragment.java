82fcaed4ec8000bada83d4954b318f1946907a52
long9ka <long9ka@gmail.com>
2019-11-16 18:46:32 +0700

    add map

--- app/src/main/java/com/example/travelapp/ui/createTour/CreateTourFragment.java
+++ app/src/main/java/com/example/travelapp/ui/createTour/CreateTourFragment.java
@@ -9,14 +9,14 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.travelapp.MapsActivity;
 import com.example.travelapp.R;
-import com.example.travelapp.ui.ListTourActivity;
 
 public class CreateTourFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_create_tour, container, false);
-        startActivity(new Intent(getActivity(), ListTourActivity.class));
+        startActivity(new Intent(getActivity(), MapsActivity.class));
         return root;
     }
 }
