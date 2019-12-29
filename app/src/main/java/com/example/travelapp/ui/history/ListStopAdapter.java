ba8087bef8790b0a437d8f2c5cc9fd743d14aeff
haile1999 <haile3107@gmail.com>
2019-12-29 21:59:46 +0700

    + List stoppoint

---
+++ app/src/main/java/com/example/travelapp/ui/history/ListStopAdapter.java
@@ -0,0 +1,4 @@
+package com.example.travelapp.ui.history;
+
+public class ListStopAdapter {
+}
---
+++ app/src/main/java/com/example/travelapp/ui/history/StopPoint.java
@@ -0,0 +1,14 @@
+package com.example.travelapp.ui.history;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StopPoint extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stop_point2);
+    }
+}
