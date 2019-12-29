98d228c8d0d49e7a65eea4397d4661637bc2c407
long9ka <long9ka@gmail.com>
2019-12-29 10:46:32 +0700

    update detail activity

--- app/src/main/java/com/example/travelapp/ui/DetailStopPointActivity.java
+++ app/src/main/java/com/example/travelapp/ui/DetailStopPointActivity.java
@@ -6,13 +6,23 @@ import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.travelapp.R;
+import com.example.travelapp.api.service.RetrofitClient;
+import com.example.travelapp.api.service.UserService;
+import com.example.travelapp.store.UserStore;
 
 public class DetailStopPointActivity extends AppCompatActivity {
+    private UserService userService;
+    private UserStore userStore;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_stop_point);
+        
+        userStore = new UserStore(this);
+        userService = RetrofitClient.getUserService();
+        
+        String accessToken = userStore.getUser().getAccessToken();
         String Id = getIntent().getStringExtra("Id");
         /**
          * Lay chi tiet cua diem dung theo id
